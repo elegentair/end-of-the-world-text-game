e5f97e317f3c65f32627fa4c21bf72f115c43310
elegentair <49598332+elegentair@users.noreply.github.com>
2019-12-02 12:32:59 -0500

    Update game.bat

--- game.bat
+++ game.bat
@@ -9,3 +9,19 @@ echo press 2 to see how to play
 echo close this window if you do not want to play
 if %desktopselect%==1 goto game
 if %desktopselect%==2 goto info
+
+
+:game
+cls
+echo you find a note
+echo it says URGENT MESSAGE the world end is near prepare now
+echo wow you say should I belive this note
+echo to belive this message press one
+echo to not belive this message press 2
+if %desktopselect%==1 goto gamey
+if %desktopselect%==2 goto gamen
+
+
+:gamey
+cls
+echo you say gosh this not is horrible
