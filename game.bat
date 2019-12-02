40d20f5adadc9829f6a70abe21405abcd6328fd7
elegentair <49598332+elegentair@users.noreply.github.com>
2019-12-02 16:39:19 -0500

    Update game.bat

--- game.bat
+++ game.bat
@@ -5,7 +5,7 @@ title end of the world game
 echo welcome to the end of the world
 echo in this game you will try to survive the end of the world and move to and explore other planets
 echo press one to start playing
-echo press 2 to see how to play
+echo press two to see how to play
 echo close this window if you do not want to play
 if %desktopselect%==1 goto game
 if %desktopselect%==2 goto info
