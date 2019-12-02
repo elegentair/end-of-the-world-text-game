c7d7ccb0871636d57a6adfb06ff260fcaaa91a96
elegentair <49598332+elegentair@users.noreply.github.com>
2019-12-02 16:38:01 -0500

    Update game.bat

--- game.bat
+++ game.bat
@@ -17,11 +17,52 @@ echo you find a note
 echo it says URGENT MESSAGE the world end is near prepare now
 echo wow you say should I belive this note
 echo to belive this message press one
-echo to not belive this message press 2
-if %desktopselect%==1 goto gamey
-if %desktopselect%==2 goto gamen
+echo to not belive this message press two
+if %gameselect%==1 goto gamey
+if %gameselect%==2 goto gamen
 
 
 :gamey
 cls
-echo you say gosh this not is horrible
+echo you say this message seems horible
+echo I hope it is a fake note
+echo but then the world starts to shake
+echo lava surrounds you
+echo what do you do
+echo press one to get somthing super cold and make land to stand on and escape
+echo press 2 to give up
+if %gameyselect%==1 goto gameone
+if %gameyselect%==2 goto gameover
+
+
+:gamen
+cls
+echo then the earth starts to shake
+echo you think why didn't I believe that note
+echo its too late now bye earth
+echo game over
+echo to quit close this window
+
+
+:gameover
+cls
+echo you say I guess this is the end
+echo bye
+echo game over
+echo to quit close this window
+
+
+:gameone
+cls
+echo you get dry ice while you still can and nitrogen and poor it in some lava
+echo it worked you have land to stand on
+cls
+echo you wake up 
+echo you survived the rapture
+echo but what planet will you move to
+echo you have to search space
+echo you know you have to look for a space ship because you know you can not stay here
+echo you know where a space ship is
+echo it is in Cape Canaveral Florida
+echo you get in your car and drive there
+echo you stay at an abandoned hotel for the night
